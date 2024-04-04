9a094627124761af026357371a0224baba66f815
Kuznec <ilyakuzn515@mail.ru>
2024-04-04 17:06:22 +0300

    Добавил улучшенный калькулятор, способный принимать вид операции, находящаяся в последнем сегменте

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double num1, double num2, char oper)
+        {
+            if (oper == '*')
+                return $"{num1} {oper} {num2} = {num1 * num2}";
+            if (oper == '-')
+                return $"{num1} {oper} {num2} = {num1 - num2}";
+            if (oper != '+' && oper != 0)
+                return "Некорректная операция";
+            return $"{num1} + {num2} = {num1 + num2}";            
+        }
+    }
+}
