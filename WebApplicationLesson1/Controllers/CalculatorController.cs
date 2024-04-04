7cca0fa9ecec4f90422f7c07780178938d55f1cd
Kuznec <ilyakuzn515@mail.ru>
2024-04-04 16:56:52 +0300

    Добавил калькулятор складывающий переданные в последних двух сегментах числа

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int num1, int num2)
+        {
+            return $"{num1} + {num2} = {num1 + num2}";
+        }
+    }
+}
