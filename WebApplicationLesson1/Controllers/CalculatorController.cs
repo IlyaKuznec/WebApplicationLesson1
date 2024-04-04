5d7a27b33dc07dde3e125e698e6ad555dc7e9765
Kuznec <ilyakuzn515@mail.ru>
2024-04-04 17:16:43 +0300

    Kuznecov_lesson2_1_4

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, char c)
+        {            
+            if (c == '*')
+                return $"{a} {c} {b} = {a * b}";
+            if (c == '-')
+                return $"{a} {c} {b} = {a - b}";
+            if (c == '/' && b != 0)
+                return $"{a} {c} {b} = {a / b}";
+            if (c == '/' && b == 0)
+                return "Ошибка: деление на ноль.";
+            if (c != '+' && c != 0)
+                return "Некорректная операция.";
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
